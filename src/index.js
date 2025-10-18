044ef52f916fa9728679882d05eb1336ac0bc8df
Krizel Rika <krizelcodes@gmail.com>
2025-10-18 14:01:24 +0800

    adding particle design in JS

--- src/index.js
+++ src/index.js
@@ -72,4 +72,17 @@ class WeatherQuestGame {
             this.showCharacterMessage();
         });
     }
+
+     createParticles() {
+        const particlesContainer = document.getElementById('particles');
+        for (let i = 0; i < 20; i++) {
+            const particle = document.createElement('div');
+            particle.className = 'particle';
+            particle.style.left = Math.random() * 100 + '%';
+            particle.style.animationDelay = Math.random() * 6 + 's';
+            particle.style.animationDuration = (Math.random() * 3 + 3) + 's';
+            particlesContainer.appendChild(particle);
+        }
+    }
+
 }
